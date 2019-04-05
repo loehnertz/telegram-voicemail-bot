38a899e5210da26e01487c2acd8931633b6b808c
Jakob Löhnertz <mail@jakob.codes>
2019-04-05 22:30:38 +0200

    Add a generic class for uploading files to an S3-compatible hosting provider

---
+++ backend/uploader.py
@@ -0,0 +1,66 @@
+from pathlib import Path
+
+import boto3
+from botocore.exceptions import ClientError
+
+
+class Uploader:
+    S3_KEY = 's3'
+    CONTENT_TYPE_KEY = 'ContentType'
+    PUBLIC_EXTRA_ARGS = {'ACL': 'public-read'}
+
+    def __init__(self, region_name, endpoint_url, access_key, secret):
+        self.session = self.init_session()
+        self.client = self._init_client(region_name, endpoint_url, access_key, secret)
+
+    def upload_file_publicly(self, file_location, upload_path, bucket, extra_args={}, overwrite=False):
+        return self._upload_file(
+            file_location=file_location,
+            upload_path=upload_path,
+            bucket=bucket,
+            extra_args={**self.PUBLIC_EXTRA_ARGS, **extra_args},
+            overwrite=overwrite,
+        )
+
+    def upload_file_privately(self, file_location, upload_path, bucket, extra_args={}, overwrite=False):
+        return self._upload_file(
+            file_location=file_location,
+            upload_path=upload_path,
+            bucket=bucket,
+            extra_args=extra_args,
+            overwrite=overwrite,
+        )
+
+    def _upload_file(self, file_location, upload_path, bucket, extra_args={}, overwrite=False):
+        path = Path(file_location).resolve()
+        file_upload_path = f'{upload_path}/{str(path.name)}'
+
+        if overwrite is False and self._file_already_exists(file_upload_path, bucket):
+            raise FileExistsError(f"The file at upload path '{file_upload_path}' already exists")
+
+        return self.client.upload_file(
+            str(path),
+            str(bucket),
+            file_upload_path,
+            ExtraArgs=extra_args,
+        )
+
+    def _file_already_exists(self, file_path, bucket):
+        try:
+            self.client.head_object(Key=file_path, Bucket=bucket)
+            return True
+        except ClientError:
+            return False
+
+    def _init_client(self, region_name, endpoint_url, access_key, secret_key):
+        return self.session.client(
+            service_name=self.S3_KEY,
+            region_name=region_name,
+            endpoint_url=endpoint_url,
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key,
+        )
+
+    @staticmethod
+    def init_session():
+        return boto3.session.Session()
